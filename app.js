af16f56f1c69de2008485c35a46800762b3d3f60
jeff0518 <cycivs07@icloud.com>
2022-08-24 13:56:03 +0800

    Add searchType

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+
 // Express
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -14,16 +15,23 @@ app.get('/', (req, res) => {
 	res.render('index', { restaurant: restaurantList.results })
 })
 
-// 搜索功能 新增找不到資料頁面
+// 搜索功能 新增找不到資料的頁面
 app.get('/search/', (req, res) => {
-	const keyword = req.query.keyword
+	const searchType = req.query.searchType
+	const keyword = req.query.keyword.trim().toLowerCase()
 	const restaurant = restaurantList.results.filter(restaurant => {
-		return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+		if (searchType === 'name') {
+			return restaurant.name.toLowerCase().includes(keyword)
+		} else if (searchType === 'category') {
+			return restaurant.category.toLowerCase().includes(keyword)
+		} else if (searchType === 'location') {
+			return restaurant.location.toLowerCase().includes(keyword)
+		}
 	})
 	if (restaurant.length > 0) {
-		res.render('index', { restaurant: restaurant, keyword: keyword })
+		res.render('index', { restaurant: restaurant, keyword: keyword, searchType: searchType })
 	} else {
-		res.render('error', { restaurant: restaurant, keyword: keyword })
+		res.render('error', { restaurant: restaurant, keyword: keyword, searchType: searchType })
 	}
 })
 
