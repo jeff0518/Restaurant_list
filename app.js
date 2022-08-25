208c5ee7cb41dd8981250856a641453c4583681e
jeff0518 <cycivs07@icloud.com>
2022-08-25 20:21:30 +0800

    Optimization <select> Click on the item, after pressing the search button, it will stay in the selected item

--- app.js
+++ app.js
@@ -19,19 +19,25 @@ app.get('/', (req, res) => {
 app.get('/search/', (req, res) => {
 	const searchType = req.query.searchType
 	const keyword = req.query.keyword.trim().toLowerCase()
+	let getSelected1 = ''
+	let getSelected2 = ''
+	let getSelected3 = ''
 	const restaurant = restaurantList.results.filter(restaurant => {
 		if (searchType === 'name') {
+			getSelected1 = 'selected'
 			return restaurant.name.toLowerCase().includes(keyword)
 		} else if (searchType === 'category') {
+			getSelected2 = 'selected'
 			return restaurant.category.toLowerCase().includes(keyword)
 		} else if (searchType === 'location') {
+			getSelected3 = 'selected'
 			return restaurant.location.toLowerCase().includes(keyword)
 		}
 	})
 	if (restaurant.length > 0) {
-		res.render('index', { restaurant: restaurant, keyword: keyword, searchType: searchType })
+		res.render('index', { restaurant: restaurant, keyword: keyword, searchType: searchType, selected1: getSelected1, selected2: getSelected2, selected3: getSelected3 })
 	} else {
-		res.render('error', { restaurant: restaurant, keyword: keyword, searchType: searchType })
+		res.render('error', { restaurant: restaurant, keyword: keyword, searchType: searchType, selected1: getSelected1, selected2: getSelected2, selected3: getSelected3 })
 	}
 })
 
